170db319281c26e940b548ee30b9116816de42de
abhishekibr2 <abhishekibr.trainee2@gmail.com>
2024-12-28 18:55:26 +0530

    Optimised

--- components/env-var-warning.tsx
+++ components/env-var-warning.tsx
@@ -16,7 +16,7 @@ export function EnvVarWarning() {
           disabled
           className="opacity-75 cursor-none pointer-events-none"
         >
-          <Link href="/sign-in">Sign in</Link>
+          <a href="/sign-in">Sign in</a>
         </Button>
         <Button
           asChild
@@ -25,7 +25,7 @@ export function EnvVarWarning() {
           disabled
           className="opacity-75 cursor-none pointer-events-none"
         >
-          <Link href="/sign-up">Sign up</Link>
+          <a href="/sign-up">Sign up</a>
         </Button>
       </div>
     </div>
